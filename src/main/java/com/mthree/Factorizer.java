72624a9f49b11761fe6f48f33d1f86ae14caacb2
antonbaider <email.b.anton@gmail.com>
2024-10-06 18:42:10 +0200

    Commit2

--- src/main/java/com/mthree/Factorizer.java
+++ src/main/java/com/mthree/Factorizer.java
@@ -1,5 +1,15 @@
 package com.mthree;
 
+/**
+ * This program must be in a new console application named Factorizer.
+ * This program must ask the user for the number the program will factor.
+ * The program must print out the original number.
+ * The program must print out each factor of the number (not including the number itself).
+ * The program must print out the total number of factors for the number.
+ * The program must print out whether or not the number is perfect.
+ * The program must print out whether or not the number is prime.
+ */
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
