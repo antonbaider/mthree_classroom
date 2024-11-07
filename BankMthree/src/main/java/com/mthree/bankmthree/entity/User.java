d401ec3a1ff7fdfaeb342b33c6aa4d56c8521ff6
Anton <antonbaider@icloud.com>
2024-11-07 09:38:43 +0100

    ver 14 (Userdetails)

--- BankMthree/src/main/java/com/mthree/bankmthree/entity/User.java
+++ BankMthree/src/main/java/com/mthree/bankmthree/entity/User.java
@@ -11,8 +11,12 @@ import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -28,7 +32,7 @@ import java.util.Set;
 @AllArgsConstructor
 @Builder
 @EntityListeners(AuditingEntityListener.class)
-public class User {
+public class User implements UserDetails {
 
     /**
      * Primary key for the User entity.
@@ -195,4 +199,29 @@ public class User {
     @EqualsAndHashCode.Exclude
     @ToString.Exclude
     private Set<Account> accounts;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
